b3f628305fe0293baaa1c082ec363e4f9e7362bf
Marcus Crane <marcus@thingsima.de>
2017-02-05 02:18:32 +1300

    Started a simple classes project

---
+++ SimpleClasses/SimpleClasses/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SimpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie favouriteMovie = new Movie();
+
+            favouriteMovie.Name = "Bronson";
+            favouriteMovie.Director = "Nicolas Winding Refn";
+            favouriteMovie.Year = 2008;
+            favouriteMovie.Budget = 230000;
+
+            Console.WriteLine("I can recommend {0} by {1}!\nIt released in {2} and was made on a budget of ${3}.",
+                favouriteMovie.Name,
+                favouriteMovie.Director,
+                favouriteMovie.Year,
+                favouriteMovie.Budget
+                );
+
+            Console.ReadKey();
+        }
+    }
+
+    class Movie
+    {
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public int Year { get; set; }
+        public int Budget { get; set; }
+    }
+}
