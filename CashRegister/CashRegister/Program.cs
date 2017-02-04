bdcbe52be7a76f077d3c2d8adc099562a5f2cc61
Marcus Crane <marcus@thingsima.de>
2017-02-04 23:03:40 +1300

    Added user interaction

--- CashRegister/CashRegister/Program.cs
+++ CashRegister/CashRegister/Program.cs
@@ -6,11 +6,25 @@ namespace CashRegister
     {
         static void Main(string[] args)
         {
-            const double applePrice = 2.99;
-            const double bananaPrice = 3.99;
-            const double orangePrice = 4.99;
+            double applePrice, bananaPrice, orangePrice, total;
+            int appleQuantity, bananaQuantity, orangeQuantity;
 
-            Console.WriteLine("The price of buying one of each fruit would be {0}", applePrice + bananaPrice + orangePrice);
+            applePrice = 2.99;
+            bananaPrice = 3.99;
+            orangePrice = 4.99;
+
+            Console.WriteLine("How many apples are you purchasing?");
+            appleQuantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many bananas are you purchasing?");
+            bananaQuantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How many oranges are you purchasing");
+            orangeQuantity = int.Parse(Console.ReadLine());
+
+            total = (applePrice * appleQuantity) + (bananaPrice * bananaQuantity) + (orangePrice * orangeQuantity);
+
+            Console.WriteLine("That's {0} apples, {1} bananas and {2} oranges for a total of ${3}", appleQuantity, bananaQuantity, orangeQuantity, total);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
