78a9ce13ef6d95356f0fced903c2c2928aa6e8d0
Marcus Crane <marcus@thingsima.de>
2017-02-04 21:53:32 +1300

    Basic program works

--- CashRegister/CashRegister/Program.cs
+++ CashRegister/CashRegister/Program.cs
@@ -6,6 +6,13 @@ namespace CashRegister
     {
         static void Main(string[] args)
         {
+            const double applePrice = 2.99;
+            const double bananaPrice = 3.99;
+            const double orangePrice = 4.99;
+
+            Console.WriteLine("The price of buying one of each fruit would be {0}", applePrice + bananaPrice + orangePrice);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
