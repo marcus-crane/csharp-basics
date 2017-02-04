dea9994b472828244e6ee28c3f92465a28fdeb6c
Marcus Crane <marcus@thingsima.de>
2017-02-05 00:45:12 +1300

    Expanded out into a second method and did a bit of cleaning

--- SimpleMethod/SimpleMethod/Program.cs
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -8,17 +8,43 @@ namespace SimpleMethod
 {
     class Program
     {
-        // Main is the entrypoint of our program
+        // Main is the entrypoint of our program / void means the function itself returns nothing and it can accept arguments
         static void Main(string[] args)
         {
-            HelloWorld();
+            string firstName, lastName, homeTown;
+
+            Console.WriteLine("Welcome! Tell me about yourself?");
+
+            Console.Write("What is your first name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("What is your last name: ");
+            lastName = Console.ReadLine();
+
+            Console.Write("What is your hometown: ");
+            homeTown = Console.ReadLine();
+
+            Respond(firstName, lastName, homeTown);
+
             Console.WriteLine("Press any key to exit...");
-            Console.ReadLine();
+            Console.ReadKey();
         }
 
-        private static void HelloWorld()
+        // Respond method is private so it can't be accessed outside of the Program class(?) and is void so it returns no value
+        private static void Respond(string firstName, string lastName, string homeTown)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Did you know that your first name backwards is {0}?", ReverseString(firstName));
+            Console.WriteLine("Did you know that your last name backwards is {0}?", ReverseString(lastName));
+            Console.WriteLine("Did you know that the name of your hometown backwards is {0}?", ReverseString(homeTown));
+        }
+
+        // ReverseString method is private and returns a string
+        private static string ReverseString(string message)
+        {
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+
+            return string.Concat(messageArray).ToLower();
         }
     }
 }
