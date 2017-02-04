7693b5ecba5add71292bced9d7ad3ba605fcaab0
Marcus Crane <marcus@thingsima.de>
2017-02-04 23:40:48 +1300

    Added a simple hello world method rather than relying on Main()

---
+++ SimpleMethod/SimpleMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMethod
+{
+    class Program
+    {
+        // Main is the entrypoint of our program
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
