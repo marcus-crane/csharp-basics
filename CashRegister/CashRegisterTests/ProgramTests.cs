1275856bbffe348239e33e500e50e867cca961e1
Marcus Crane <marcus@thingsima.de>
2017-02-04 21:35:35 +1300

    Added xUnit and wrote a test that passes to prove it works

--- CashRegister/CashRegisterTests/ProgramTests.cs
+++ CashRegister/CashRegisterTests/ProgramTests.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Xunit;
 
 namespace CashRegisterTests
 {
-    public class Class1
+    public class ProgramTests
     {
+        [Fact]
+        public void Working()
+        {
+            Assert.True(true);
+        }
     }
 }
