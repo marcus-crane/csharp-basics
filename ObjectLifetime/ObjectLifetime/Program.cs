5e73de230fa4fbe9e5ca47ed94b2b1bf64c6a119
Marcus Crane <marcus@thingsima.de>
2017-02-05 02:32:53 +1300

    Created an ObjectLifetime project

---
+++ ObjectLifetime/ObjectLifetime/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ObjectLifetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie goodMovie = new Movie();
+        }
+    }
+
+    class Movie
+    {
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public int Year { get; set; }
+        public int Budget { get; set; }
+    }
+}
