8c4d91cd2bfe613aa012d591eed0f7feac008e38
Marcus Crane <marcus@thingsima.de>
2017-02-07 23:31:53 +1300

    Fixed some comments

--- fileWriter/fileWriter/Program.cs
+++ fileWriter/fileWriter/Program.cs
@@ -5,6 +5,7 @@ class WriteToFile
 {
     public static void Main()
     {
+        // Create a new instance of the StreamWriter class
         StreamWriter writer;
         // Creates file.txt on the desktop. @ symbol means don't interpret backslashes as control characters eg \n for newline
         writer = new StreamWriter(@"C:\Users\admin\Desktop\file.txt");
@@ -13,7 +14,7 @@ class WriteToFile
         // Closes the stream
         writer.Close();
 
-        // Open the file for reading as a stream
+        // Open the file for reading as a stream by creating a new instance of the StreamReader class
         StreamReader reader = new StreamReader("file.txt");
         // While the stream hasn't reached its end (signaled by an empty string being returned)
         while (reader.EndOfStream == false)
