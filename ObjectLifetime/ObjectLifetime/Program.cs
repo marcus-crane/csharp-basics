640162247dcd0ac4b104055f66d7bdae9926e356
seqi <tomseqi@live.co.uk>
2017-02-23 21:09:34 +0000

    Added a great movie using object initializer

--- ObjectLifetime/ObjectLifetime/Program.cs
+++ ObjectLifetime/ObjectLifetime/Program.cs
@@ -9,6 +9,17 @@ namespace ObjectLifetime
             // We can just simply define our values at the time of initalisation which is much cleaner!
             Movie goodMovie = new Movie("Silence of the Lambs", "Jonathon Demme", 1991, 19000000);
             Movie anotherGoodMovie = new Movie("Red Dragon", "Brett Ratner", 2002, 78000000);
+
+            // Sometimes you want to initialise values that aren't constructor params
+            // You can do this with object initlaization.
+            Movie aGreatMovie = new Movie
+            {
+                Name = "The Room",
+                Director = "Tommy Wiseau",
+                Year = 2003,
+                Budget = 6000000,
+                Rating = 10
+            };
         }
     }
 
@@ -19,6 +30,7 @@ namespace ObjectLifetime
         public string Director { get; set; }
         public int Year { get; set; }
         public int Budget { get; set; }
+        public int Rating { get; set; }
 
         // We can also define a constructor in order to initialise our object from the start rather than manually setting each element after an instance is created
         public Movie(string name, string director, int year, int budget)
